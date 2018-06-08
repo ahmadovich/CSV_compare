58e4fc9ba527da3279da035f8830df5e97844a34
Ahmad Hamdy <ahmad.hamdy@gmail.com>
2018-06-08 14:57:09 +0200

    Adding important comments
    Adding needed comments so that code can be revisited later and
    understood in an easy way.

--- compare.py
+++ compare.py
@@ -6,17 +6,21 @@ def parsecsv(myfile):
     
     # parse the CSV file into a dictionary and retrun it
     with open(myfile, mode='r') as infile:
+        # Each row read from the csv file is returned as a list of strings
         reader = csv.reader(infile)
         for rows in reader:
+            # Convert strings to upper case, in case the two files has different case
+            # This is the key, the first column in each row
             k = rows[0].upper()
+            # if this row contains more than one column
             if len(rows) > 1:
                 v = rows[1].upper()
             else:
-                # if the CSV file contains only one column (There will be no value for the key, then create a dummy value
+                # if this row contains only one column (There will be no value for the key, then create a dummy value
                 rows.append('Null value')
                 v = rows[1].upper()
             
-            
+            # Create dictionary, for each row: key is first column, value is second column
             mydict[k] = v
         
         return mydict
@@ -28,13 +32,16 @@ parser.add_argument('-o', '--output', required = False,default = 'output.csv', t
 args = parser.parse_args()
 
 def main():
+    
+    #launch my created function parsecsv (on reference file), it returns a dictionary (reference dictionary)
     ref = parsecsv(args.reference)
+    #launch my created function parsecsv (on users file), it returns a dictionary (users dictionary)
     lookup = parsecsv(args.findlist)
     with open(args.output,'w') as outfile:
         # Compare key in dictionaries to find which key exists in the two dictionaries
         for x in lookup.keys():
             if x in ref.keys():
-                
+                # Manually construct the CSV lines
                 linez = '\"' + x + '\"' + ',' + ref[x]
                 outfile.write(linez + '\n')
                
